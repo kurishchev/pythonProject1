8fa265311b9c568e7255cd024907e70463280078
kurishchev <kurishchev@rambler.ru>
2024-07-07 21:17:06 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -1,8 +1,8 @@
 def parole(a):
     par_e = str()
     for i in range(1, a-1):
-        for k in range(1, a-i):
-            if a % i+k == 0:
+        for k in range(i+1, a-1):
+            if a % (i+k) == 0:
                 par_e = par_e + str(i)
                 par_e = par_e + str(k)
     return par_e
