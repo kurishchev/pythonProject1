38319e7c282e184f475fcf01aac466440782f947
kurishchev <kurishchev@rambler.ru>
2024-07-11 19:58:14 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -4,7 +4,7 @@ b = int(input('Введите число из первой вставки:  '))
 def parole(a):
     par_e = str()
     for i in range(1, a-1):
-        for k in range(i+1, a-1):
+        for k in range(i+1, a):
             if a % (i+k) == 0:
                 par_e = par_e + str(i)
                 par_e = par_e + str(k)
